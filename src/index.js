715962896372fadfbbe5a3983eda90fcb4164757
Zo Andrianambinina <zo@livenexx.net>
2021-11-25 16:10:37 +0300

    affichage posts & users

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { getPosts } from "./actions/post.action";
+import { getUsers } from "./actions/user.action";
 import App from "./App";
+import rootReducer from "./reducers";
 import "./styles/index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.dispatch(getPosts());
+store.dispatch(getUsers());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
